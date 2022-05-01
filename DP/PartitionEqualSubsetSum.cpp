64b431ada4de48dd2bd2682740fc5906a304b1b9
ankushk1729 <ankushrattan17@gmail.com>
2022-05-01 16:00:09 +0530

    DP - PartitionEqualSubsetSum.cpp - Add more comments

--- DP/PartitionEqualSubsetSum.cpp
+++ DP/PartitionEqualSubsetSum.cpp
@@ -18,6 +18,7 @@ bool checkSubsetSum(int k,vector<int>& nums){
 
 	vector<bool> prev(k+1,0),curr(k+1,0);
 
+	// Because for every index i if target sum is 0 then it is a valid solution.
 	prev[0] = curr[0] = true;
 
 	if(nums[0] <= k) prev[nums[0]] = true;
@@ -49,8 +50,10 @@ int main(){
 
 	for(int i = 0;i<n;i++) totalSum += nums[i];
 
+	// If total sum is odd then it can't be divided into two equal parts.
 	if(totalSum % 2 != 0) return false;
 
+	// The two subarrays should have equal sum that means both of them would be having the sum as half of whole array sum
 	int k = totalSum / 2;
 
 	return checkSubsetSum(k,nums);
